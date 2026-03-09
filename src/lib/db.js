c1178d40b41f3a2459d2e4b813f39a16ffd2938d
Anuj Maurya <rishabmaurya097@gmail.com>
2026-03-09 15:10:29 +0530

    fix db export for vercel

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,7 +1,5 @@
 import { Pool } from "pg";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL + "?sslmode=verify-full"
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL + "?sslmode=verify-full",
 });
-
-export default pool;
